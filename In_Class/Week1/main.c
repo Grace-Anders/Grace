f8ef10bc18b3b5d4a22fdfade7e36ab09445786b
Austin-Derrick <Austinderrick15@gmail.com>
2022-02-01 12:40:15 -0600

    work?

--- In_Class/Week1/main.c
+++ In_Class/Week1/main.c
@@ -6,6 +6,20 @@
 //Run App:
 // ./app
 
+// How printf works _____________
+
+void real_printf(const char *s)
+{
+    while(*s != '\0')
+    {
+        putchar(*s);
+        s++;
+    }
+}
+// ______________________________
+
+void foo(const char *str) { printf("%s", str); }
+
 //to not pass though anything () has to have "void" otherwise it can accept anything!!
 int main(void){
 
@@ -14,11 +28,15 @@ int main(void){
         and a collection of arguments (ex: "Hello World")
     */
     printf("%s", "\tHello World\n\0");
-
+    foo("Hello World\0");
     /* 
         \n newline
         \t (place before) tab
-        \0
+        \0 terminates a string (some comilers will add this automatically, but it is still best practice)
+
+        * overloaded opperated pointer or 
+        derefrence (goes to memory adress and grabs what is there)
     */
 }
+
  
