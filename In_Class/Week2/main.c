6b493ac0feab1f934656a85771ab151470335812
Austin-Derrick <Austinderrick15@gmail.com>
2022-02-01 13:29:11 -0600

    Week 2 CP

--- In_Class/Week2/main.c
+++ In_Class/Week2/main.c
@@ -1,8 +1,83 @@
-//#import <stdio.h>
 #include <stdio.h>
+
+//Compile: (no output is good!)
+//gcc main.c -o app 
+
+//Proper Compile Statment:
+//gcc -o app -std=c99 -Wall -Wextra -Werror -pedantic -O2 main.c
+
+/* main_ : in main module 
+void main_foo(void);
+void main_bar(int);
+int main_baz(int);
+*/
+
+//Function that takes in any pointer | void is a type in C (dose not mean nothing)
+//void *get(void * id){}
+
+/*Pass by Value
+
+    C# MS: Most everything is passed by value
+ */
+void by_value(int n){
+    n++;
+}
+
+/*Pass by Refrence
+
+   C# MS: If and instance of a class is passed it is considered a refrence type
+*/
+void by_refrence(int *n)
+{
+    (*n)++;
+}
 
 int main(void)
 {
     //print Hello World
-    printf("%s", "Hello World\n\0");
+    printf("%s", "Hello World\n");
+
+    /*if loop _______________________________________
+
+    if(1 == 1)//No booleans in C only 0 and 1
+    {
+        printf("%s", "The World Makes Sense");
+    }
+    else if(1 == 2){
+        printf("%s", "Please Stop");
+    }
+    else{
+       printf("%s", "What on Earth have you Done!!"); 
+    }
+
+    //for loop _____________________________________
+
+    for (int i = 0; i < 10; i++)  //i not working currently
+    {
+        printf("%s", "Ping");
+    }
+    
+    //while loop ___________________________________
+
+    while(1==1){
+        printf("%s", "runs forever");
+    }
+
+    do{
+        printf("%s", "runs once");
+    }while (1==2);
+    */
+
+   int n = 100;
+
+   by_value(n);
+
+   printf("%d", n);
+
+   by_refrence(&n);
+
+   printf("%d", n);
+
+
 }
+
